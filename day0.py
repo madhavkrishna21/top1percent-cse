91780e288c101dc7960e9b54ceff0872d628d43b
madhavkrishna21 <madhavkrishna.rg@gmail.com>
2025-12-04 22:39:10 +0530

    Delete day0.py

--- day0.py
+++
@@ -1,9 +0,0 @@
-# day0.py - My 1% Journey Starts
-def fut_ceo():
-    name = input("Enter your name: ")
-    goal = input("Your 2030 goal (₹1Cr / AI Startup): ")
-    print(f"\n{name}, welcome to the 1%.")
-    print(f"Goal: {goal}")
-    print("Daily proof begins NOW.")
-
-fut_ceo()
